777cb2a5923508399223b0d2e6b0a6ca8bca2874
Zulfikar <fikar27@gmail.com>
2023-06-30 17:51:35 +0800

    okde

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToHome() {
+    window.location.href = "index.html"; // Ganti dengan URL halaman utama yang sesuai
+}
+
+function goToAbout() {
+    window.location.href = "about.html"; // Ganti dengan URL halaman tentang yang sesuai
+}
